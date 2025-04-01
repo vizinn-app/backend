8bffea25b37f55fa9af149073cc8b66d4388522f
lmFreire <l.matheus.freire10@gmail.com>
2025-04-01 20:15:08 -0300

    fix: abstracting SMS sending logic into an external function

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,9 @@
-import random
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from twilio.rest import Client
 
 from backend.database import get_session
 from backend.models import User, UserVerification
@@ -18,15 +16,9 @@ from backend.schemas import (
     verifyCodeSchema,
 )
 from backend.security import create_access_token, get_password_hash, verify_password
-from backend.settings import Settings
+from helper.utils import send_sms
 
 app = FastAPI()
-
-account_sid = 'AC86990c387fd4133bc6de6657430e3293'
-auth_token = 'badf430b8957bd7a1c99caebaed9f158'
-twilio_phone_number = '+12394280641'
-
-client = Client(Settings().account_sid, Settings().auth_token)
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
@@ -50,34 +42,7 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
     session.commit()
     session.refresh(db_user)
 
-    verification_code = f'{random.randint(100000, 999999)}'
-
-    user_verification = session.scalar(
-        select(UserVerification).where(UserVerification.user_id == db_user.id)
-    )
-
-    if user_verification:
-        user_verification.verification_code = verification_code
-        user_verification.is_verified = False
-    else:
-        user_verification = UserVerification(
-            user_id=db_user.id, verification_code=verification_code, is_verified=False
-        )
-        session.add(user_verification)
-
-    session.commit()
-
-    try:
-        client.messages.create(
-            body=f'Your verification code is: {verification_code}',
-            from_=Settings().twilio_phone_number,
-            to=f'+55{user.phone}',
-        )
-    except Exception as e:
-        raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            detail=f'Failed to send SMS: {str(e)}',
-        )
+    send_sms(db_user, session)
 
     return db_user
 
